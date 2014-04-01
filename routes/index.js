eb9b264e502e751ed4af3d868309a96bb2cc90af
lusaisai <lusaisai@163.com>
2014-04-01 11:09:38 +0800

    show group list

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,49 @@
 exports.index = function(req, res){
-	var users = db.collection('users');
-	users.find({email: { $in: req.user.friends }}).toArray(function (err, result) {
-		if ( err ) {
-			res.send(500);
-		} else {
-			res.render('index', {me: req.user, friends: result});
-		}
+	friend_list(req, res, function() {
+		group_list(req, res, function () {
+			res.render('index', {me: req.user, friends: req.friends, groups: req.groups});
+		})
 	});
-	
 };
 
 exports.friend_list = function (req, res) {
+	friend_list(req, res);
+};
+
+exports.group_list = function (req, res) {
+	group_list(req, res);
+};
+
+
+var friend_list = function (req, res, next) {
 	var users = db.collection('users');
-	users.find({email: { $in: req.user.friends }}).toArray(function (err, result) {
+	users.find({email: { $in: req.user.friends }}).toArray(function (err, docs) {
 		if ( err ) {
 			res.send(500);
 		} else {
-			res.render('friend_list', {friends: result});
+			if(next){
+				req.friends = docs;
+				next();
+			} else {
+				res.render('friend_list', {friends: docs});
+			}
 		}
 	});
-}
+};
+
+
+var group_list = function (req, res, next) {
+	var groups = db.collection('groups');
+	groups.find({members: req.user.email}).toArray(function (err, docs) {
+		if ( err ) {
+			res.send(500);
+		} else {
+			if (next) {
+				req.groups = docs;
+				next();
+			} else{
+				res.render('group_list', {groups: docs});
+			}
+		}
+	});
+};
