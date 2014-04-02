f6e9e99b59b518d71761a440fd97c2e327996f09
lusaisai <lusaisai@163.com>
2014-04-02 12:11:54 +0800

    index

--- mongodb/index.js
+++ mongodb/index.js
@@ -20,7 +20,38 @@ exports.init = function (next) {
             process.exit(1);;
         } else{
             global.db = db;
-            next();
+            var empty = function(){};
+            db.collection('users').ensureIndex({email:1}, {background:true}, empty);
+            db.collection('users').ensureIndex({last_show_time:1}, {background:true}, empty);
+            db.collection('users').ensureIndex({friends:1}, {background:true}, empty);
+            db.collection('users').ensureIndex({name:1}, {background:true}, empty);
+
+            db.collection('groups').ensureIndex({members:1}, {background:true}, empty);
+            db.collection('groups').ensureIndex({name:1}, {background:true}, empty);
+            db.collection('groups').ensureIndex({owner:1}, {background:true}, empty);
+
+            db.collection('messages').ensureIndex({from:1}, {background:true}, empty);
+            db.collection('messages').ensureIndex({to:1}, {background:true}, empty);
+            db.collection('messages').ensureIndex({from:1, to:1}, {background:true}, empty);
+            db.collection('messages').ensureIndex({time:1}, {background:true}, empty);
+
+            db.collection('message_histories').ensureIndex({from:1}, {background:true}, empty);
+            db.collection('message_histories').ensureIndex({to:1}, {background:true}, empty);
+            db.collection('message_histories').ensureIndex({from:1, to:1}, {background:true}, empty);
+            db.collection('message_histories').ensureIndex({time:1}, {background:true}, empty);
+            
+            db.collection('group_messages').ensureIndex({from:1}, {background:true}, empty);
+            db.collection('group_messages').ensureIndex({to:1}, {background:true}, empty);
+            db.collection('group_messages').ensureIndex({from:1, to:1}, {background:true}, empty);
+            db.collection('group_messages').ensureIndex({time:1}, {background:true}, empty);
+            db.collection('group_messages').ensureIndex({not_received:1}, {background:true}, empty);
+            
+            db.collection('group_message_histories').ensureIndex({from:1}, {background:true}, empty);
+            db.collection('group_message_histories').ensureIndex({to:1}, {background:true}, empty);
+            db.collection('group_message_histories').ensureIndex({from:1, to:1}, {background:true}, empty);
+            db.collection('group_message_histories').ensureIndex({time:1}, {background:true}, empty);
+            
+            next(); 
         }
     });
 };
